c75f9421d029a8a62d5b833a77672e1d0864060e
SARTHAK MISHRA <147376925+Sarthakmishra001@users.noreply.github.com>
2026-01-27 18:14:33 +0530

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import Cursor from "./components/CustomCursor"; // 👈 ADD THIS
 
 const queryClient = new QueryClient();
 
@@ -13,10 +14,12 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+
+      <Cursor /> {/* 👈 GLOBAL CURSOR */}
+
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
